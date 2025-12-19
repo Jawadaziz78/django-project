0010a7c4b4cb1a7c34ae5970347aaa9958a0f09b
Ubuntu <ubuntu@ip-172-31-77-148.ec2.internal>
2025-12-19 05:43:03 +0000

    Fix: Added dynamic base path for stage deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/nextjs',
-  assetPrefix: '/nextjs',
+  // 1. Dynamic Base Path
+  basePath: process.env.BASE_URL || '',
+  
+  // 2. Asset Prefix
+  assetPrefix: process.env.BASE_URL || '',
+
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
